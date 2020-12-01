872f1fa187bc27d70ddb71f55a22bcdd07527967
promobius <matt.huebert@gmail.com>
2020-12-01 13:42:51 -0600

    conversation between ron swanson and kayne west

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using Newtonsoft.Json.Linq;
+
+namespace KayneandRon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+
+
+            for(int i = 0; i < 10; i++)
+            {
+                QuoteGenerator.KanyeQuote();
+                Thread.Sleep(3000);
+                QuoteGenerator.RonQuote();
+                Thread.Sleep(3000);
+            }
+         
+         
+        }
+       
+            
+    }
+}
---
+++ QuoteGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace KayneandRon
+{
+    public class QuoteGenerator
+    {
+        public static void KanyeQuote()
+        {
+            var client = new HttpClient();
+            var kayneURL = "https://api.kanye.rest";
+            var kanyeResponse = client.GetStringAsync(kayneURL).Result;
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            Console.WriteLine($"Kanye: {kanyeQuote}");
+
+        }
+
+        public static void RonQuote()
+        {
+            var client1 = new HttpClient();
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var ronResponse = client1.GetStringAsync(ronURL).Result;
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            Console.WriteLine($"Ron: {ronQuote}");
+        }
+    }
+}
